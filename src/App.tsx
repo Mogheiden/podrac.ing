874884699bc7aee21363ba09f993cf6bfde7b1a9
Mogheiden <mogheiden@gmail.com>
2023-12-17 21:44:14 +1100

    basic movement & pngs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import bishopImg from './chussy/pieces/bishop.png';
+import pawnImg from './chussy/pieces/pawn.png';
+import rookImg from './chussy/pieces/rook.png';
+import knightImg from './chussy/pieces/knight.png';
+import queenImg from './chussy/pieces/queen.png';
+import kingImg from './chussy/pieces/king.png';
 import './App.css';
 import {
   getChussBoard,
@@ -9,7 +15,24 @@ import {
   ChussPieceType,
 } from './chussy/chuss';
 
-const squareSize = 60;
+const squareSize = 75;
+
+function getChussPieceImage(type: ChussPieceType) {
+  switch (type) {
+    case 'Pawn':
+      return pawnImg;
+    case 'Knight':
+      return knightImg;
+    case 'Rook':
+      return rookImg;
+    case 'Bishop':
+      return bishopImg;
+    case 'Queen':
+      return queenImg;
+    case 'King':
+      return kingImg;
+  }
+}
 
 function Square(props: {
   y: number;
@@ -30,7 +53,16 @@ function Square(props: {
         display: 'inline-block',
       }}
     >
-      {props.type}
+      {props.type && (
+        <img
+          src={getChussPieceImage(props.type)}
+          style={{
+            width: '100%',
+            height: '100%',
+            filter: props.side === 'Black' ? 'invert(1)' : undefined,
+          }}
+        />
+      )}
     </div>
   );
 }
--- src/chussy/chuss.ts
+++ src/chussy/chuss.ts
@@ -2,6 +2,8 @@ export function getChussBoard() {
   return [];
 }
 
+const boardsize = 8;
+
 export type ChussPieceType =
   | 'Pawn'
   | 'Knight'
@@ -76,4 +78,131 @@ export function newBoard(): ChussBoard {
   ];
 
   return board;
+}
+
+export function possibleMoves(
+  board: ChussBoard,
+  [row, col]: [row: number, column: number]
+): [row: number, column: number][] {
+  const moveArray: [number, number][] = [];
+  const piece = board[row][col];
+  if (!piece) {
+    throw new Error("There's no piece there bucko!");
+  }
+
+  const allowMovementInDirections = (
+    directions: [number, number][],
+    singleStep?: boolean
+  ) => {
+    directions.forEach(([dRow, dCol]) => {
+      let currentRow = row;
+      let currentCol = col;
+
+      let sentinel = true;
+
+      while (sentinel) {
+        currentCol += dCol;
+        currentRow += dRow;
+
+        if (
+          currentCol < 0 ||
+          currentRow < 0 ||
+          currentCol > boardsize ||
+          currentRow > boardsize
+        ) {
+          sentinel = false;
+        }
+
+        const thingAtPos = board[currentRow][currentCol];
+
+        if (!thingAtPos) {
+          moveArray.push([currentRow, currentCol]);
+        } else if (thingAtPos.pieceSide != piece.pieceSide) {
+          moveArray.push([currentRow, currentCol]);
+          sentinel = false;
+        } else {
+          sentinel = false;
+        }
+
+        if (singleStep) {
+          sentinel = false;
+        }
+      }
+    });
+  };
+  if (piece.pieceType === 'Rook') {
+    allowMovementInDirections([
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+    ]);
+  } else if (piece.pieceType === 'Bishop') {
+    allowMovementInDirections([
+      [1, 1],
+      [1, -1],
+      [-1, 1],
+      [-1, -1],
+    ]);
+  } else if (piece.pieceType === 'Queen') {
+    allowMovementInDirections([
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+      [1, 1],
+      [1, -1],
+      [-1, 1],
+      [-1, -1],
+    ]);
+  } else if (piece.pieceType === 'King') {
+    allowMovementInDirections(
+      [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0],
+        [1, 1],
+        [1, -1],
+        [-1, 1],
+        [-1, -1],
+      ],
+      true
+    );
+  } else if (piece.pieceType === 'Knight') {
+    allowMovementInDirections(
+      [
+        [2, 1],
+        [2, -1],
+        [1, 2],
+        [-1, 2],
+        [-2, 1],
+        [-2, -1],
+        [-1, -2],
+        [1, -2],
+      ],
+      true
+    );
+  } else if (piece.pieceType === 'Pawn') {
+    const direction: number = piece.pieceSide == 'White' ? -1 : 1;
+    if (!board[row + 1 * direction][col]) {
+      moveArray.push([row + 1 * direction, col]);
+      if (!piece.pieceMoved && !board[row + 2 * direction][col]) {
+        moveArray.push([row + 2 * direction, col]);
+      }
+    }
+    if (
+      board[row + 1 * direction][col + 1] &&
+      board[row + 1 * direction][col + 1]?.pieceSide !== piece.pieceSide
+    ) {
+      moveArray.push([row + 1 * direction, col + 1]);
+    }
+    if (
+      board[row + 1 * direction][col - 1] &&
+      board[row + 1 * direction][col - 1]?.pieceSide !== piece.pieceSide
+    ) {
+      moveArray.push([row + 1 * direction, col - 1]);
+    }
+  }
+  return moveArray;
 }
