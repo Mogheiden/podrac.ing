9e993d036060a929fe6e7960aa87098a58294708
Mogheiden <mogheiden@gmail.com>
2024-01-05 20:17:54 +1100

    Add the schmeat

--- src/pages/home/home_page.tsx
+++ src/pages/home/home_page.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import kino from './podracing.mp4';
 
 export function HomePage() {
   return (
@@ -6,6 +7,17 @@ export function HomePage() {
       now this is podrac.ing
       <br />
       <Link to="chuss">Chuss</Link>
+      <br />
+      <video
+        style={{ width: '90vw' }}
+        ref={(e) => e?.play(0)}
+        src={kino}
+        autoPlay
+        loop
+        controls
+        playsInline
+        muted
+      />
     </div>
   );
 }
