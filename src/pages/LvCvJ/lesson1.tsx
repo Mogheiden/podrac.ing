0ca3b3d15f94f121460f72e047abbcbcc7448f2b
Mogheiden <mogheiden@gmail.com>
2024-01-14 22:33:04 +1100

    image display

--- src/pages/LvCvJ/lesson1.tsx
+++ src/pages/LvCvJ/lesson1.tsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
+
 export function Lesson1() {
+  const [file, setFile] = useState<string | undefined>(undefined);
   return (
     <div>
       <div>insert flavour text here</div>
       <div>
         <input
           type="file"
-          onChange={(e) => console.log(e.target.files?.item(0))}
+          onChange={(e) => {
+            setFile(undefined);
+            const reader = new FileReader();
+            const file = e.target.files?.item(0);
+            if (!file) {
+              return;
+            }
+            reader.onload = (evt) => setFile(evt.target?.result?.toString());
+            reader.readAsDataURL(file);
+          }}
+        />
+      </div>
+      <div>
+        <img
+          src={file}
+          style={{
+            maxWidth: '100%',
+            maxHeight: 512,
+          }}
         />
       </div>
     </div>
