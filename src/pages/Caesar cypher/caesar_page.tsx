913a9b1c07d2bcf8cf1ab99990fa29adec016a3d
Mogheiden <mogheiden@gmail.com>
2024-01-23 22:42:05 +1100

    finalization

--- src/pages/Caesar cypher/caesar_page.tsx
+++ src/pages/Caesar cypher/caesar_page.tsx
@@ -2,9 +2,38 @@ import { useState } from 'react';
 import { caesarDecoder, caesarShift } from './caesar_decoder';
 
 export function CaesarPage() {
+  return (
+    <div>
+      <EncodingSection />
+      <DecodingSection />
+    </div>
+  );
+}
+
+function DecodingSection() {
+  const [code, setCode] = useState('');
+  return (
+    <div>
+      Write your Cummeiform script to "de-cummiefy":
+      <br />
+      <textarea
+        name="Input"
+        rows={4}
+        cols={40}
+        value={code}
+        placeholder="Put your Cummeiform in my box!"
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <br />
+      <div>Common text!</div>
+      <div>{caesarDecoder(code)}</div>
+    </div>
+  );
+}
+
+function EncodingSection() {
   const [input, setInput] = useState('Write your common script in this box!');
   const [shift, setShift] = useState('69');
-  const [code, setCode] = useState('');
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       Write your common script to "Cummeify":
@@ -28,18 +57,36 @@ export function CaesarPage() {
       <div>Cummeified text!</div>
       <div>{caesarShift(input, parseInt(shift))}</div>
       <br />
-      Write your Cummeiform script to "de-cummiefy":
-      <textarea
-        name="Input"
-        rows={4}
-        cols={40}
-        value={code}
-        placeholder="Put your Cummeiform in my box!"
-        onChange={(e) => setCode(e.target.value)}
-      />
-      <br />
-      <div>Common text!</div>
-      <div>{caesarDecoder(code)}</div>
     </div>
   );
 }
+
+// function UncontrolledDecodingSection() {
+//   const [output, setOutput] = useState('');
+//   return (
+//     <>
+//       <form
+//         method="post"
+//         onSubmit={(e) => {
+//           e.preventDefault();
+//           const formData = new FormData(e.target as HTMLFormElement);
+//           setOutput(caesarDecoder(formData.get('postContent')));
+//         }}
+//       >
+//         <label>
+//           Edit your post:
+//           <textarea
+//             name="postContent"
+//             defaultValue="I really enjoyed biking yesterday!"
+//             rows={4}
+//             cols={40}
+//           />
+//         </label>
+//         <hr />
+//         <button type="reset">Reset edits</button>
+//         <button type="submit">Save post</button>
+//       </form>
+//       <div>{output}</div>
+//     </>
+//   );
+// }
