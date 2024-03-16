90f1b1b29d01c2d55ab39bc649ffe6ed60f762ea
Mogheiden <mogheiden@gmail.com>
2024-03-16 23:23:33 +1100

    added custom colours

--- src/pages/WordCloud/WordCloud.tsx
+++ src/pages/WordCloud/WordCloud.tsx
@@ -9,6 +9,13 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
 } from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 
@@ -19,8 +26,11 @@ function makeSortedArray(input: string, commonWords: Set<string>) {
     while (/[^a-zA-Z]$/.test(word.slice(-1))) {
       word = word.slice(0, -1);
     }
-    if (word.endsWith('s') && !word.endsWith('ss')) {
-      word = word.slice(0, -1);
+    if (word.length < 2 || commonWords.has(word)) {
+      return;
+    }
+    if (word.endsWith("'s")) {
+      word = word.slice(0, -2);
     }
     if (/[^a-zA-Z0-9]$/.test(word[0])) {
       word = word.substring(1);
@@ -40,6 +50,49 @@ function makeSortedArray(input: string, commonWords: Set<string>) {
   return [wordMap, top100, wordOccurrences] as const;
 }
 
+const allColourCodes = [
+  '#22272b',
+  '#495054',
+  '#cdd3d6',
+  '#ebebeb',
+  '#004000',
+  '#00aa45',
+  '#a8edb3',
+  '#dbfadf',
+  '#0b3f47',
+  '#2e808e',
+  '#8cdbe5',
+  '#d1eeea',
+  '#002664',
+  '#146cfd',
+  '#8ce0ff',
+  '#cbedfd',
+  '#441170',
+  '#8055f1',
+  '#cebfff',
+  '#e6e1fd',
+  '#65004d',
+  '#d912ae',
+  '#f4b5e6',
+  '#fddef2',
+  '#630019',
+  '#d7153a',
+  '#ffb8c1',
+  '#ffe6ea',
+  '#941b00',
+  '#f3631b',
+  '#FFCE99',
+  '#fdeddf',
+  '#694800',
+  '#faaf05',
+  '#fde79a',
+  '#fff4cf',
+  '#523719',
+  '#b68d5d',
+  '#e8d0b5',
+  '#ede3d7',
+];
+
 const colorSchemes = {
   Kingfisher: ['#146cfd', '#002664', '#8ce0ff', '#8055f1'],
   Waratah: ['#d7153a', '#941b00', '#f3631b', '#faaf05'],
@@ -50,8 +103,19 @@ type ColorScheme = keyof typeof colorSchemes;
 export function WordCloud() {
   const textField = useRef('');
   const [submittedText, submitText] = useState('');
-  const [colorSchemeKey, setColorScheme] = useState<ColorScheme>('Kingfisher');
-  const colorScheme = colorSchemes[colorSchemeKey];
+  const [colorSchemeKey, setColorScheme] = useState<ColorScheme | 'Custom'>(
+    'Kingfisher'
+  );
+  const [customColorScheme, setCustomColorScheme] = useState<string[]>([]);
+  const [customColorSchemeInput, setCustomColorSchemeInput] = useState<
+    string[]
+  >([]);
+
+  const [modal, setModal] = useState<boolean>(false);
+  const colorScheme =
+    colorSchemeKey === 'Custom'
+      ? customColorScheme
+      : colorSchemes[colorSchemeKey];
   const [wordMap, words, wordCount] = makeSortedArray(
     submittedText,
     commonWords
@@ -82,7 +146,7 @@ export function WordCloud() {
         <Button onClick={() => print()}>Print</Button>
         <Menu>
           <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-            Color Palette: {colorSchemeKey}
+            Colour Palette: {colorSchemeKey}
           </MenuButton>
           <MenuList>
             {Object.entries(colorSchemes).map(([schemeKey, scheme]) => (
@@ -104,6 +168,20 @@ export function WordCloud() {
                 ))}
               </MenuItem>
             ))}
+            <MenuItem onClick={() => setModal(true)}>
+              Custom &nbsp;&nbsp;
+              {customColorScheme?.map((color) => (
+                <span
+                  style={{
+                    height: 16,
+                    width: 16,
+                    backgroundColor: color,
+                    marginRight: 6,
+                    borderRadius: 5,
+                  }}
+                />
+              ))}
+            </MenuItem>
           </MenuList>
         </Menu>
       </ButtonGroup>
@@ -180,6 +258,79 @@ export function WordCloud() {
           </div>
         ))}
       </div>
+      <Modal isOpen={modal} onClose={() => setModal(false)}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Custom Colour Scheme</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {new Array(4).fill(0).map((_, i) => (
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rightIcon={<ChevronDownIcon />}
+                  style={{ width: 180, marginRight: 10, marginBottom: 10 }}
+                >
+                  Colour {i + 1}:{' '}
+                  <span
+                    style={{
+                      position: 'absolute',
+                      display: 'inline-block',
+                      height: 24,
+                      width: 24,
+                      backgroundColor: customColorSchemeInput[i],
+                      marginLeft: 6,
+                      borderRadius: 5,
+                      marginTop: -3,
+                    }}
+                  />
+                </MenuButton>
+                <MenuList
+                  style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    minWidth: 'unset',
+                    maxWidth: 124,
+                  }}
+                >
+                  {allColourCodes.map((color) => (
+                    <MenuItem
+                      onClick={() =>
+                        setCustomColorSchemeInput((arr) => {
+                          arr = structuredClone(arr);
+                          arr[i] = color;
+                          return arr;
+                        })
+                      }
+                      style={{
+                        height: 24,
+                        width: 24,
+                        backgroundColor: color,
+                        marginRight: 3,
+                        marginLeft: 3,
+                        marginBottom: 6,
+                        borderRadius: 5,
+                      }}
+                    ></MenuItem>
+                  ))}
+                </MenuList>
+              </Menu>
+            ))}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              onClick={() => {
+                setCustomColorScheme(customColorSchemeInput);
+                setColorScheme('Custom');
+                setModal(false);
+              }}
+            >
+              Set Colour Scheme
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
