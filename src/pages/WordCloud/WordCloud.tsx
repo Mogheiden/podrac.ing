31ad186da4e234fd2a6b7238dff34084603604c3
Mogheiden <mogheiden@gmail.com>
2024-03-23 17:13:49 +1100

    Refactor word cloud and fix re-renders

--- src/pages/WordCloud/WordCloud.tsx
+++ src/pages/WordCloud/WordCloud.tsx
@@ -1,5 +1,3 @@
-import { System } from 'detect-collisions';
-import { commonWords } from './commonWords';
 import { useRef, useState } from 'react';
 import {
   Button,
@@ -25,93 +23,10 @@ import {
   NumberInputStepper,
 } from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
+import { WordCloudBox } from './WordCloudBox';
+import { ColorScheme, allColourCodes, colorSchemes } from './colorSchemes';
 
-function makeSortedArray(
-  input: string,
-  filter: Set<string>,
-  commonWords: Set<string>,
-  maxWords: number
-) {
-  const wordMap = new Map<string, number>();
-  input.split(' ').forEach((word) => {
-    word = word.toLocaleLowerCase();
-    while (/[^a-zA-Z]$/.test(word.slice(-1))) {
-      word = word.slice(0, -1);
-    }
-    if (word.length < 2 || commonWords.has(word)) {
-      return;
-    }
-    if (word.endsWith("'s")) {
-      word = word.slice(0, -2);
-    }
-    if (/[^a-zA-Z0-9]$/.test(word[0])) {
-      word = word.substring(1);
-    }
-    if (word.length > 2 && !commonWords.has(word) && !filter.has(word)) {
-      wordMap.set(word, (wordMap.get(word) ?? 0) + 1);
-    }
-  });
-  console.log(wordMap);
-  const top = Array.from(wordMap.keys())
-    .sort((a, b) => wordMap.get(b)! - wordMap.get(a)!)
-    .slice(0, maxWords);
-  const wordOccurrences = top.reduce(
-    (sum, current) => sum + wordMap.get(current)!,
-    0
-  );
-  return [wordMap, top, wordOccurrences] as const;
-}
 const defaultNumberWords = 100;
-
-const allColourCodes = [
-  '#22272b',
-  '#495054',
-  '#cdd3d6',
-  '#ebebeb',
-  '#004000',
-  '#00aa45',
-  '#a8edb3',
-  '#dbfadf',
-  '#0b3f47',
-  '#2e808e',
-  '#8cdbe5',
-  '#d1eeea',
-  '#002664',
-  '#146cfd',
-  '#8ce0ff',
-  '#cbedfd',
-  '#441170',
-  '#8055f1',
-  '#cebfff',
-  '#e6e1fd',
-  '#65004d',
-  '#d912ae',
-  '#f4b5e6',
-  '#fddef2',
-  '#630019',
-  '#d7153a',
-  '#ffb8c1',
-  '#ffe6ea',
-  '#941b00',
-  '#f3631b',
-  '#FFCE99',
-  '#fdeddf',
-  '#694800',
-  '#faaf05',
-  '#fde79a',
-  '#fff4cf',
-  '#523719',
-  '#b68d5d',
-  '#e8d0b5',
-  '#ede3d7',
-];
-
-const colorSchemes = {
-  Kingfisher: ['#146cfd', '#002664', '#8ce0ff', '#8055f1'],
-  Waratah: ['#d7153a', '#941b00', '#f3631b', '#faaf05'],
-  Wollemi: ['#004000', '#00aa45', '#a8edb3', '#2e808e'],
-} as const;
-type ColorScheme = keyof typeof colorSchemes;
 
 export function WordCloud() {
   const textField = useRef('');
@@ -129,21 +44,11 @@ export function WordCloud() {
   >([]);
 
   const [modal, setModal] = useState<boolean>(false);
+
   const colorScheme =
     colorSchemeKey === 'Custom'
       ? customColorScheme
       : colorSchemes[colorSchemeKey];
-  const [wordMap, words, wordCount] = makeSortedArray(
-    submittedText,
-    submittedFilter,
-    commonWords,
-    maxWords
-  );
-
-  const system = new System();
-
-  const width = 800;
-  const height = 450;
 
   return (
     <div
@@ -261,78 +166,12 @@ export function WordCloud() {
         </Menu>
       </ButtonGroup>
       <br></br> <br></br>
-      <div
-        className="printable"
-        style={{
-          width,
-          height,
-          position: 'relative',
-          border: '1px lightgrey solid',
-          borderRadius: 5,
-          background: 'whitesmoke',
-        }}
-      >
-        {words.map((word, i) => (
-          <div
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              padding: 3,
-              fontFamily: 'Public Sans',
-              fontSize: getFontSize(i, wordMap.get(word)!, wordCount),
-              lineHeight: '100%',
-              color: colorScheme[i % colorScheme.length],
-            }}
-            ref={(el) => {
-              if (!el) return;
-              const { width: wordWidth, height: wordHeight } =
-                el.getBoundingClientRect();
-              const initialBoxPos = { x: width / 2, y: height / 2 };
-              if (i === 0) {
-                initialBoxPos.x -= wordWidth / 2;
-                initialBoxPos.y -= wordHeight / 2;
-              }
-              const box = system.createBox(
-                initialBoxPos,
-                wordWidth,
-                wordHeight
-              );
-              let numOutOfBounds = 0;
-              while (numOutOfBounds < 200) {
-                let dx = 0.5 - Math.random();
-                let dy = 0.5 - Math.random();
-                const l = Math.sqrt(dx * dx + dy * dy);
-                dx /= l;
-                dy /= l;
-                while (system.checkOne(box)) {
-                  box.setPosition(box.pos.x + 10 * dx, box.pos.y + 10 * dy);
-                }
-                if (
-                  box.pos.x + wordWidth > width ||
-                  box.pos.x < 0 ||
-                  box.pos.y + wordHeight > height ||
-                  box.pos.y < 0
-                ) {
-                  box.setPosition(initialBoxPos.x, initialBoxPos.y);
-                  numOutOfBounds += 1;
-                  continue;
-                }
-                break;
-              }
-              if (numOutOfBounds >= 20) {
-                system.remove(box);
-                return;
-              }
-              box.isStatic = true;
-              el.style.left = `${box.pos.x}px`;
-              el.style.top = `${box.pos.y}px`;
-            }}
-          >
-            {word}
-          </div>
-        ))}
-      </div>
+      <WordCloudBox
+        text={submittedText}
+        filteredWords={submittedFilter}
+        maxWords={maxWords}
+        colorScheme={colorScheme}
+      />
       <Modal isOpen={modal} onClose={() => setModal(false)}>
         <ModalOverlay />
         <ModalContent>
@@ -392,7 +231,6 @@ export function WordCloud() {
               </Menu>
             ))}
           </ModalBody>
-
           <ModalFooter>
             <Button
               onClick={() => {
@@ -408,24 +246,4 @@ export function WordCloud() {
       </Modal>
     </div>
   );
-}
-
-function getFontSize(index: number, wordFrequency: number, wordCount: number) {
-  let initialSize = Math.floor(1500 * (wordFrequency / wordCount));
-  if (initialSize < 10) {
-    initialSize = 10;
-  }
-  if (initialSize > 50) {
-    initialSize = 50;
-  }
-  if (index === 0) {
-    return initialSize + 20;
-  }
-  if (index === 1 || index === 2) {
-    return initialSize + 15;
-  }
-  if (index < 2 && index < 10) {
-    return initialSize + 5;
-  }
-  return initialSize;
 }
---
+++ src/pages/WordCloud/WordCloudBox.tsx
@@ -0,0 +1,159 @@
+import { memo } from 'react';
+import { System } from 'detect-collisions';
+import { commonWords } from './commonWords';
+
+function makeSortedArray(
+  input: string,
+  filter: Set<string>,
+  commonWords: Set<string>,
+  maxWords: number
+) {
+  const wordMap = new Map<string, number>();
+  input.split(' ').forEach((word) => {
+    word = word.toLocaleLowerCase();
+    while (/[^a-zA-Z]$/.test(word.slice(-1))) {
+      word = word.slice(0, -1);
+    }
+    if (word.length < 2 || commonWords.has(word)) {
+      return;
+    }
+    if (word.endsWith("'s")) {
+      word = word.slice(0, -2);
+    }
+    if (/[^a-zA-Z0-9]$/.test(word[0])) {
+      word = word.substring(1);
+    }
+    if (word.length > 2 && !commonWords.has(word) && !filter.has(word)) {
+      wordMap.set(word, (wordMap.get(word) ?? 0) + 1);
+    }
+  });
+  console.log(wordMap);
+  const top = Array.from(wordMap.keys())
+    .sort((a, b) => wordMap.get(b)! - wordMap.get(a)!)
+    .slice(0, maxWords);
+  const wordOccurrences = top.reduce(
+    (sum, current) => sum + wordMap.get(current)!,
+    0
+  );
+  return [wordMap, top, wordOccurrences] as const;
+}
+
+export const WordCloudBox = memo(
+  ({
+    text,
+    filteredWords,
+    maxWords,
+    colorScheme,
+  }: {
+    text: string;
+    filteredWords: Set<string>;
+    maxWords: number;
+    colorScheme: readonly string[];
+  }) => {
+    const [wordMap, words, wordCount] = makeSortedArray(
+      text,
+      filteredWords,
+      commonWords,
+      maxWords
+    );
+
+    const system = new System();
+    const width = 800;
+    const height = 450;
+
+    return (
+      <div
+        className="printable"
+        style={{
+          width,
+          height,
+          position: 'relative',
+          border: '1px lightgrey solid',
+          borderRadius: 5,
+          background: 'whitesmoke',
+        }}
+      >
+        {words.map((word, i) => (
+          <div
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              padding: 3,
+              fontFamily: 'Public Sans',
+              fontSize: getFontSize(i, wordMap.get(word)!, wordCount),
+              lineHeight: '100%',
+              color: colorScheme[i % colorScheme.length],
+            }}
+            ref={(el) => {
+              if (!el) return;
+              const { width: wordWidth, height: wordHeight } =
+                el.getBoundingClientRect();
+              const initialBoxPos = { x: width / 2, y: height / 2 };
+              if (i === 0) {
+                initialBoxPos.x -= wordWidth / 2;
+                initialBoxPos.y -= wordHeight / 2;
+              }
+              const box = system.createBox(
+                initialBoxPos,
+                wordWidth,
+                wordHeight
+              );
+              let numOutOfBounds = 0;
+              while (numOutOfBounds < 200) {
+                let dx = 0.5 - Math.random();
+                let dy = 0.5 - Math.random();
+                const l = Math.sqrt(dx * dx + dy * dy);
+                dx /= l;
+                dy /= l;
+                while (system.checkOne(box)) {
+                  box.setPosition(box.pos.x + 10 * dx, box.pos.y + 10 * dy);
+                }
+                if (
+                  box.pos.x + wordWidth > width ||
+                  box.pos.x < 0 ||
+                  box.pos.y + wordHeight > height ||
+                  box.pos.y < 0
+                ) {
+                  box.setPosition(initialBoxPos.x, initialBoxPos.y);
+                  numOutOfBounds += 1;
+                  continue;
+                }
+                break;
+              }
+              if (numOutOfBounds >= 20) {
+                system.remove(box);
+                return;
+              }
+              box.isStatic = true;
+              el.style.left = `${box.pos.x}px`;
+              el.style.top = `${box.pos.y}px`;
+            }}
+          >
+            {word}
+          </div>
+        ))}
+      </div>
+    );
+  }
+);
+
+function getFontSize(index: number, wordFrequency: number, wordCount: number) {
+  let initialSize = Math.floor(1500 * (wordFrequency / wordCount));
+  if (initialSize < 10) {
+    initialSize = 10;
+  }
+  if (initialSize > 50) {
+    initialSize = 50;
+  }
+  if (index === 0) {
+    return initialSize + 20;
+  }
+  if (index === 1 || index === 2) {
+    return initialSize + 15;
+  }
+  if (index < 2 && index < 10) {
+    return initialSize + 5;
+  }
+  return initialSize;
+}
